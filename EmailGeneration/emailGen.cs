2ff37bc040329213a2d2a7535541d64d4e1989a9
Simon O'Hara <SOHara@dv02.co.uk>
2017-08-02 16:27:45 +0100

    Add comments, define exceptions for email generation failure

--- EmailGeneration/emailGen.cs
+++ EmailGeneration/emailGen.cs
@@ -1,22 +1,34 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Runtime.InteropServices;
 using System.Web.UI.WebControls;
 
+// Args[] expected array composition
+// args[0] Username
+// args[1] User Number
+// args[2] Date
+// args[3] Amount Exceeded (Data or £s)
+// args[4] Customer
+// args[5] Subject
+// args[6] File Path
 
 namespace Email
 {
     public class EmailGeneration
     {
+        public MailMessage EmailMessage { get; private set; }
+
         public void Gen([In, MarshalAs(UnmanagedType.LPArray, SizeConst = 7)]string[] args)
         {
 
-            string[] CustomerKey = new string[] { "DV02", "Grunenthal", "OPEL", "OPUK", "Cleanbrite", "Nordea" };
-            string CKeyResult = CustomerKey.FirstOrDefault(s => args[4].Contains(s));
+            string[] CustomerKey = new string[] { "DV02", "Grunenthal", "OPEL", "OPUK", "Cleanbrite", "Nordea" }; // Define customers using service to match on
+            string CKeyResult = CustomerKey.FirstOrDefault(s => args[4].Contains(s)); // Collect first match and place it in CKeyResult
             string CustomerEmail = string.Empty;
 
+            //Set customer enail address defined on contents of CKeyResult
             switch (CKeyResult)
             {
                 case "DV02":
@@ -39,13 +51,16 @@ namespace Email
                     break;
             }
             
-            string emailBody = File.ReadAllText(args[6]);
+            string emailBody = File.ReadAllText(args[6]); //Read email body text and place in variable emailBody
+
             MailDefinition md = new MailDefinition()
             {
                 From = "noreply@dv02.co.uk",
                 IsBodyHtml = true,
                 Subject = args[5]
             };
+
+            //Defines variables in email body and what arguments they will be replaced with
             ListDictionary replacements = new ListDictionary
             {
                 { "{name}", args[0] },
@@ -53,10 +68,24 @@ namespace Email
                 { "{date}", args[2] },
                 { "{Amount_Exceeded}", args[3] }
             };
-            MailMessage msg = md.CreateMailMessage(CustomerEmail, replacements, emailBody, new System.Web.UI.Control());
-            //               msg.To.Add(new MailAddress(CustomerEmail));
-            //               msg.Body = emailBody;
 
+            try
+            {
+                // Creates email message in variable msg
+                EmailMessage = CreateEmail(CustomerEmail, emailBody, md, replacements);
+            }
+            catch (NullReferenceException e) // Exception thrown if variables not defined for email message creation
+            {
+                Console.WriteLine("Error: Email variables not defined");
+                Console.WriteLine(e.ToString());
+            }
+            catch (Exception e) //Exception thrown for undefined error
+            {
+                Console.WriteLine("An unknown error has occurred:");
+                Console.WriteLine(e.ToString());
+            }
+            
+            //Define SMTP Connection
             SmtpClient client = new SmtpClient()
             {
                 Port = 25,
@@ -64,19 +93,15 @@ namespace Email
                 UseDefaultCredentials = false,
                 Host = "dv02-co-uk.mail.protection.outlook.com"
             };
-            client.Send(msg);
 
+            // Send email
+            client.Send(EmailMessage);
+        }
 
-
-            // Args[] expected array composition
-            // args[0] Username
-            // args[1] User Number
-            // args[2] Date
-            // args[3] Amount Exceeded (Data or £s)
-            // args[4] Customer
-            // args[5] Subject
-            // args[6] File Path
-
+        private static MailMessage CreateEmail(string CustomerEmail, string emailBody, MailDefinition md, ListDictionary replacements)
+        {
+            MailMessage msg = md.CreateMailMessage(CustomerEmail, replacements, emailBody, new System.Web.UI.Control());
+            return msg;
         }
     }
 }
