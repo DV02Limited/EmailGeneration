e69d7709746bb6ac2002a207523b8df740e138d4
Simon O'Hara <SOHara@dv02.co.uk>
2017-08-07 10:21:44 +0100

    Added method for sending email generation error email to hdcalls.

--- EmailGeneration/emailGen.cs
+++ EmailGeneration/emailGen.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.IO;
-using System.Linq;
 using System.Net.Mail;
 using System.Runtime.InteropServices;
 using System.Web.UI.WebControls;
@@ -18,75 +17,79 @@ using System.Web.UI.WebControls;
 namespace Email
 {
     public enum Customers { DV02, Grunenthal, OPEL, OPUK, Cleanbrite, Nordea};
+    
 
     public class EmailGeneration
     {
         public MailMessage EmailMessage { get; private set; }
+        public static string CustomerEmail { get; set; }
+        
 
-        public void Gen([In, MarshalAs(UnmanagedType.LPArray, SizeConst = 7)]string[] args)
+        public int Gen([In, MarshalAs(UnmanagedType.LPArray, SizeConst = 7)]string[] args)
         {
-            string CustomerEmail;
 
-            //Set customer enail address defined on contents of CKeyResult
-            switch (Enum.Parse(typeof(Customers), args[4]))
+            try
             {
-                case Customers.DV02:
-                    CustomerEmail = "hdcalls@dv02.co.uk";
-                    break;
-                case Customers.Grunenthal:
-                    CustomerEmail = "uk.ithelpdesk@grunenthal.com";
-                    break;
-                case Customers.OPEL:
-                    CustomerEmail = "itsupport@otsuka-europe.com";
-                    break;
-                case Customers.OPUK:
-                    CustomerEmail = "itsupport@otsuka-europe.com";
-                    break;
-                case Customers.Cleanbrite:
-                    CustomerEmail = "customercare@cleanbrite.co.uk";
-                    break;
-                case Customers.Nordea:
-                    CustomerEmail = "dlldlondonit@nordea.com";
-                    break;
-                default:
-                    CustomerEmail = null;
-                    break;
+                //Set customer enail address defined on contents of CKeyResult
+                switch (Enum.Parse(typeof(Customers), args[4]))
+                {
+                    case Customers.DV02:
+                        CustomerEmail = "hdcalls@dv02.co.uk";
+                        break;
+                    case Customers.Grunenthal:
+                        CustomerEmail = "uk.ithelpdesk@grunenthal.com";
+                        break;
+                    case Customers.OPEL:
+                        CustomerEmail = "itsupport@otsuka-europe.com";
+                        break;
+                    case Customers.OPUK:
+                        CustomerEmail = "itsupport@otsuka-europe.com";
+                        break;
+                    case Customers.Cleanbrite:
+                        CustomerEmail = "customercare@cleanbrite.co.uk";
+                        break;
+                    case Customers.Nordea:
+                        CustomerEmail = "dlldlondonit@nordea.com";
+                        break;
+                    default:
+                        CustomerEmail = null;
+                        break;
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Customer name {args[4]} does not match");
+                Console.WriteLine(e.ToString());
+                return 1;
             }
 
             string emailBody = File.ReadAllText(args[6]); //Read email body text and place in variable emailBody
 
-            MailDefinition md = new MailDefinition()
-            {
-                From = "noreply@dv02.co.uk",
-                IsBodyHtml = true,
-                Subject = args[5]
-            };
+                MailDefinition md = new MailDefinition()
+                {
+                    From = "noreply@dv02.co.uk",
+                    IsBodyHtml = true,
+                    Subject = args[5]
+                };
 
-            //Defines variables in email body and what arguments they will be replaced with
-            ListDictionary replacements = new ListDictionary
-            {
-                { "{name}", args[0] },
-                { "{mobile}", args[1] },
-                { "{date}", args[2] },
-                { "{Amount_Exceeded}", args[3] }
-            };
+                //Defines variables in email body and what arguments they will be replaced with
+                ListDictionary replacements = new ListDictionary
+                 {
+                    { "{name}", args[0] },
+                    { "{mobile}", args[1] },
+                    { "{date}", args[2] },
+                    { "{Amount_Exceeded}", args[3] }
+                 };
 
-            try
-            {
-                // Creates email message in variable msg
-                EmailMessage = CreateEmail(CustomerEmail, emailBody, md, replacements);
-            }
-            catch (NullReferenceException e) // Exception thrown if variables not defined for email message creation
-            {
-                Console.WriteLine("Error: Email variables not defined");
-                Console.WriteLine(e.ToString());
-            }
-            catch (Exception e) //Exception thrown for undefined error
-            {
-                Console.WriteLine("An unknown error has occurred:");
-                Console.WriteLine(e.ToString());
-            }
-            
+            // Creates email message in variable msg
+            CreateEmail(CustomerEmail, emailBody, md, replacements);
+            return 0;
+        }
+
+        private static void CreateEmail(string CustomerEmail, string emailBody, MailDefinition md, ListDictionary replacements)
+        {
+            MailMessage msg = md.CreateMailMessage(CustomerEmail, replacements, emailBody, new System.Web.UI.Control());
+
             //Define SMTP Connection
             SmtpClient client = new SmtpClient()
             {
@@ -97,13 +100,40 @@ namespace Email
             };
 
             // Send email
-            client.Send(EmailMessage);
+            client.Send(msg);
         }
 
-        private static MailMessage CreateEmail(string CustomerEmail, string emailBody, MailDefinition md, ListDictionary replacements)
+        public int CreateErrorEmail(string emailBody)
         {
-            MailMessage msg = md.CreateMailMessage(CustomerEmail, replacements, emailBody, new System.Web.UI.Control());
-            return msg;
+            try
+            {
+                MailMessage msg = new MailMessage("noreply@dv02.co.uk", "hdcalls@dv02.co.uk")
+                {
+                    Subject = "Mobile Usage Alert Discrepancy",
+                    Body = emailBody,
+                    IsBodyHtml = true
+                };
+
+                //Define SMTP Connection
+                SmtpClient client = new SmtpClient()
+                {
+                    Port = 25,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "dv02-co-uk.mail.protection.outlook.com"
+                };
+
+                // Send email
+                client.Send(msg);
+                return 0;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Error sending email");
+                Console.WriteLine(e.ToString());
+                return 1;
+            }
+
         }
     }
 }
